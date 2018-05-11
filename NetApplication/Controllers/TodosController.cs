5df42a1234795c37ff3756efa32be660fe4e9cc9
Luis R Espinoza B <luis.espinoza@facware.com>
2018-05-10 23:02:32 -0600

    added and setup web api

---
+++ NetApplication/Controllers/TodosController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using NetApplication.Models;
+
+namespace NetApplication.Controllers
+{
+    public class TodosController : ApiController
+    {
+		Todos[] todos = new Todos[]
+		{
+			new Todos { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+			new Todos { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+			new Todos { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+		};
+
+		public IEnumerable<Todos> GetAllProducts()
+		{
+			return todos;
+		}
+
+		public IHttpActionResult GetProduct(int id)
+		{
+			var product = todos.FirstOrDefault((p) => p.Id == id);
+			if (product == null)
+			{
+				return NotFound();
+			}
+			return Ok(product);
+		}
+	}
+}
